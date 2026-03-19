de5f8b84ca3ff764d3017b629a7c282450d64020
ICEOLIS1 <iceeamigos1@gmail.com>
2026-03-19 13:44:57 -0300

    Add files via upload
    fiz em casa porque tava bugado na escola e meu pc crashou lá então reprogramei o codigo inteiro

---
+++ C. cp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    int main(){
+        //Meu nome "Cauã"
+        printf("Hello, World");
+        printf ("Cauã");
+        printf("Paes vivaldo");
+        //Oque eu (Cauã) gosto de fazer
+        printf ("basquete e vídeo game");
+        //Meu nome "Carlos"
+        printf ("Carlos")
+        //Sobrenome Tavares
+        printf("Tavares")
+        //Oque eu "Carlos" gosto
+        printf ("Carros e Casas")
+        //Meu nome "Guilherme"
+        printf("Guilherme")
+        //Oque eu "Guilherme" gosto
+        printf("fighting games")
+        //Sobrenome
+        printf("Miranda")
+         return 0;
+}
